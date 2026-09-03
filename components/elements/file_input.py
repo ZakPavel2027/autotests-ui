3be683b9d3274f0959f2f1e45f01681614f9b897
ZakPavel2027 <karandash154@gmail.com>
2026-09-03 13:29:24 +0300

    Знакомство с PageFactory

---
+++ components/elements/file_input.py
@@ -0,0 +1,8 @@
+from components.elements.base_element import BaseElement
+
+
+class FileInput(BaseElement):
+    def set_input_file(self, file: str, **kwargs):
+        locator = self.get_locator(**kwargs)
+        locator.set_input_files(file)
+        
