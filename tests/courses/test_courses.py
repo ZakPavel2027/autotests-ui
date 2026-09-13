875ad0af5b5fe4f751c4adf43d090fc81887b9dd
ZakPavel2027 <karandash154@gmail.com>
2026-09-13 12:20:46 +0300

    Настройки автотестов с Pydantic

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -23,7 +23,7 @@ class TestCourses:
     @allure.severity(Severity.NORMAL)
     def test_empty_courses_list(self, courses_list_page: CoursesListPage):
         courses_list_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-        courses_list_page.navbar.check_visible('username')
+        courses_list_page.navbar.check_visible('test2')
         courses_list_page.sidebar.check_visible()
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.check_visible_empty_view()
--- tests/dashboard/test_dashboard.py
+++ tests/dashboard/test_dashboard.py
@@ -22,7 +22,7 @@ class TestDashboard:
     @allure.severity(Severity.NORMAL)
     def test_dashboard_displaying(self, dashboard_page_with_state: DashboardPage):
         dashboard_page_with_state.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
-        dashboard_page_with_state.navbar.check_visible('username')
+        dashboard_page_with_state.navbar.check_visible('test2')
         dashboard_page_with_state.sidebar.check_visible()
         dashboard_page_with_state.dashboard_toolbar_view.check_visible()
         dashboard_page_with_state.check_visible_scores_chart()
